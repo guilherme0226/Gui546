e1909d9f4af2ae71018e18c60cc35ffb9b00074c
guilherme0226 <guilherme.2956@aluno.pr.senac.br>
2026-02-13 18:06:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//Pedir Idade
+//Comparar a idade do usuario com a lei
+//Resposta
+
+let idadeUser = prompt("Digite sua idade");
+let estadoUser = prompt("Digite seu estado:").toUpperCase();
+if (idadeUser >=18 && estadoUser === "PR") {
+    document.getElementById("lascou").innerText = "hora de dar tchau!";
+    document.getElementById("").innerText = "";
+    document.getElementById("").innerText = "";
+} else {
+    document.getElementById("lascou").innerText = "Acertou Miseravíi!";
+    document.getElementById("Voce ganhou meu chapa").innerText = "";
+    document.getElementById(" Gol do corintians ").innerText = "";
+}
+   
+
+
+
+
